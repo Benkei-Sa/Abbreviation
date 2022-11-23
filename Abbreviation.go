36a3864f2744526c2e716db0cbfbacc7df596a45
Benkei-Sa <yaroslav.buzlov@gmail.com>
2022-11-23 15:05:28 +0300

    add

---
+++ Abbreviation.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"unicode"
+
+	//	"unicode"
+	"unicode/utf8"
+)
+
+func main() {
+	phrase := readString()
+	abbr := abbreviation(phrase)
+	fmt.Println(string(abbr))
+}
+
+func abbreviation(phrase string) (abbr []rune) {
+	s := strings.Fields(phrase)
+	for _, n := range s {
+		r, _ := utf8.DecodeRuneInString(n)
+		if unicode.IsLetter(r) == true {
+			abbr = append(abbr, (unicode.ToUpper(r)))
+		}
+	}
+	return abbr
+}
+
+func readString() string {
+	rdr := bufio.NewReader(os.Stdin)
+	str, _ := rdr.ReadString('\n')
+	return str
+}
